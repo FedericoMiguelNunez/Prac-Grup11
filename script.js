9de0208476904ffdf9c5d760e54aabdbf848264d
FedericoMiguelNunez <fede24896@gmail.com>
2023-04-16 19:08:41 -0300

    terminacion del trabajo integrador

--- script.js
+++ script.js
@@ -41,7 +41,45 @@ $(document).ready(()=>{
         console.log("se dono $"+ donativo);
     })
 })
+$(document).ready(function() {
+              
+    $(".nextBtn").click(function() {
+      var PasoActual = $(this).closest(".form-group");
+      var PasoSiguiente = $(this).closest(".form-group").next();
+      PasoActual.hide();
+      PasoSiguiente.show();
+    });
 
+   
+    $(".prevBtn").click(function() {
+      var PasoActual= $(this).closest(".form-group");
+      var PasoAnterior = $(this).closest(".form-group").prev();
+      PasoActual.hide();
+      PasoAnterior.show();
+    });
+
+    
+    $("#formAdopcion").submit(function(e) {
+      e.preventDefault();
+      
+      var nombre = $("#paso1 input[placeholder='Nombre']").val();
+      var apellido = $("#paso1 input[placeholder='Apellido']").val();
+      var edad = $("#paso1 input[placeholder='Edad']").val();
+      var email = $("#paso2 input[placeholder='Correo electrónico']").val();
+      var telefono = $("#paso2 input[placeholder='Número de teléfono']").val();
+      var direccion = $("#paso2 textarea[placeholder='Dirección']").val();
+      $("#nombreConfirmacion").text(nombre);
+      $("#apellidoConfirmacion").text(apellido);
+      $("#edadConfirmacion").text(edad);
+      $("#emailConfirmacion").text(email);
+      $("#telefonoConfirmacion").text(telefono);
+      $("#direccionConfirmacion").text(direccion);
+      $("#paso3").show();
+      setTimeout(function() {
+        $("#formAdopcion").get(0).submit();
+      }, 500);
+    });
+  });
 function initMap(){
     var coord = {lat:-34.6246874 ,lng: -58.5059093};
     var map = new google.maps.Map(document.getElementById('map'),{
